05daa114ac58647b1f76ae60c43cf7de2d378263
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-13 17:43:14 +1000

    resolvers. added me query

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { User } = require("../models");
+
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+};
